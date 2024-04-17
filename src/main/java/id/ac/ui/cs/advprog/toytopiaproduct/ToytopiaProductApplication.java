ced8c53ca04c27c32bd065d4e04dbbd474063b72
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2024-04-17 16:18:19 +0700

    initialize

---
+++ src/main/java/id/ac/ui/cs/advprog/toytopiaproduct/ToytopiaProductApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.toytopiaproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToytopiaProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToytopiaProductApplication.class, args);
+    }
+
+}
