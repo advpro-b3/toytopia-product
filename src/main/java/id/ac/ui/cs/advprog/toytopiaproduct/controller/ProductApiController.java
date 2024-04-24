365a61e348396927fdf1f48fc63b671fa843859b
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2024-04-24 21:01:46 +0700

    [GREEN] feat(controller): add create endpoint on ProductApiController

--- src/main/java/id/ac/ui/cs/advprog/toytopiaproduct/controller/ProductApiController.java
+++ src/main/java/id/ac/ui/cs/advprog/toytopiaproduct/controller/ProductApiController.java
@@ -5,6 +5,7 @@ import id.ac.ui.cs.advprog.toytopiaproduct.service.ProductServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -21,5 +22,18 @@ public class ProductApiController {
     @GetMapping("/product/{productId}")
     public Product getProductById(@PathVariable String productId) {
         return productService.findById(productId);
+    }
+
+    @PostMapping("/create-product")
+    public Product createProduct(@RequestBody HashMap<String, String> productAttributes) {
+
+        Product product = new Product.ProductBuilder(productAttributes.get("name"))
+                .setDescription(productAttributes.get("description"))
+                .setPrice(Integer.parseInt(productAttributes.get("price")))
+                .setStock(Integer.parseInt(productAttributes.get("stock")))
+                .setDiscount(Integer.parseInt(productAttributes.get("discount")))
+                .setAvailability(productAttributes.get("availability"))
+                .build();
+        return productService.create(product);
     }
 }
