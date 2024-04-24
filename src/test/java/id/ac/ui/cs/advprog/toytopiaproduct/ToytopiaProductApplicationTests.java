06074a6a7df6e3fd6abe4a59f4bcc296204cf991
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2024-04-24 17:27:09 +0700

    [GREEN] test(app): delete app test

--- src/test/java/id/ac/ui/cs/advprog/toytopiaproduct/ToytopiaProductApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/toytopiaproduct/ToytopiaProductApplicationTests.java
@@ -1,13 +1,8 @@
 package id.ac.ui.cs.advprog.toytopiaproduct;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ToytopiaProductApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
