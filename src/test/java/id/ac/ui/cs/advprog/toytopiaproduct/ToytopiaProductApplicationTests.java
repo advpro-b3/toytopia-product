63395a40aba85b6f1a892781d536b3ae83f680b8
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2024-04-26 21:08:22 +0700

    [GREEN] test(app): make app test

--- src/test/java/id/ac/ui/cs/advprog/toytopiaproduct/ToytopiaProductApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/toytopiaproduct/ToytopiaProductApplicationTests.java
@@ -1,8 +1,18 @@
 package id.ac.ui.cs.advprog.toytopiaproduct;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 class ToytopiaProductApplicationTests {
+    @Test
+    void contextLoads() {
+    }
 
+    @Test
+    void mainTest(){
+        assertDoesNotThrow(() -> ToytopiaProductApplication.main(new String[]{}));
+    }
 }
