7f2d6be65f13b293167b11dc52d0a96f46d1f85b
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2024-05-13 17:22:43 +0700

    test(application): increase coverage on application test

--- src/test/java/id/ac/ui/cs/advprog/toytopiaproduct/ToytopiaProductApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/toytopiaproduct/ToytopiaProductApplicationTests.java
@@ -1,18 +1,26 @@
 package id.ac.ui.cs.advprog.toytopiaproduct;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ToytopiaProductApplicationTests {
-    @Test
-    void contextLoads() {
-    }
+
+    @Autowired
+    private ToytopiaProductApplication toytopiaProductApplication;
 
     @Test
-    void mainTest(){
-        assertDoesNotThrow(() -> ToytopiaProductApplication.main(new String[]{}));
+    void contextLoads() {
+        assertNotNull(toytopiaProductApplication);
+    }
+
+    @SuppressWarnings("static-access")
+    @Test
+    void mainTest() {
+        assertDoesNotThrow(() -> toytopiaProductApplication.main(new String[]{}));
     }
 }
