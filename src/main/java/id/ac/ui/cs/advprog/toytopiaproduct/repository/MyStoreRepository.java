743f6797d447ac33dd96ef131afc9348906d6e77
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2024-05-26 22:24:04 +0700

    fix(application): delete unused files

--- src/main/java/id/ac/ui/cs/advprog/toytopiaproduct/repository/MyStoreRepository.java
+++
@@ -1,8 +0,0 @@
-package id.ac.ui.cs.advprog.toytopiaproduct.repository;
-
-import id.ac.ui.cs.advprog.toytopiaproduct.model.MyStore;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MyStoreRepository extends JpaRepository<MyStore, Long> {
-    MyStore findMyStoreById(Long userId);
-}
